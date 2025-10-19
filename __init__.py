697d76f35a6d12bb43ca15374fdad2c1a6a62ab2
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-10-19 00:44:26 -0400

    Update version number to 1.0.0

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 bl_info = {
     "name": "N Item line up",
     "author": "Lancine Doumbia",
-    "version": (0, 1, 0),
+    "version": (1, 0, 0),
     "blender": (2, 8, 0),
     "location": "View3D > Sidebar",
     "description": "Lies up the selected amount of objects along an axis",
